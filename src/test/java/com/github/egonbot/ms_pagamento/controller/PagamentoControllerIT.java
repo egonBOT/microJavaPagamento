138a1904dd78390391f9d412444899b179bf4268
labsfiap <egonthor@gmail.com>
2025-04-23 09:17:16 -0300

    Finalização dos Teste

--- src/test/java/com/github/egonbot/ms_pagamento/controller/PagamentoControllerIT.java
+++ src/test/java/com/github/egonbot/ms_pagamento/controller/PagamentoControllerIT.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.egonbot.ms_pagamento.dto.PagamentoDTO;
 import com.github.egonbot.ms_pagamento.tests.Factory;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -12,8 +13,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -94,6 +94,102 @@ public class PagamentoControllerIT {
                 .andExpect(jsonPath("$.valor").exists())
                 .andExpect(jsonPath("$.nome").value(pagamentoDTO.getNome()))
                 .andExpect(jsonPath("$.status").value("CRIADO"));
+
+    }
+    @Test
+    public void createShouldPersistPagamentoWithRequiredFields() throws Exception {
+
+        pagamentoDTO = Factory.createNewPagamentoDTOWithRequiredFields();
+        String jsonRequestBody = objectMapper.writeValueAsString(pagamentoDTO);
+        mockMvc.perform(post("/pagamentos")
+                        .content(jsonRequestBody)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(header().exists("Location"))
+                .andExpect(jsonPath("$.id").exists())
+                .andExpect(jsonPath("$.valor").exists())
+                .andExpect(jsonPath("$.valor").value(pagamentoDTO.getValor()))
+                .andExpect(jsonPath("$.status").value("CRIADO"))
+                .andExpect(jsonPath("$.nome").isEmpty())
+                .andExpect(jsonPath("$.validade").isEmpty());
+    }
+
+    @Test
+    @DisplayName("create  deve lançar  expetion quando os dados estiverem errados")
+    public void createShouldThrowsExceptionWhenInvalidData() throws Exception{
+
+        pagamentoDTO = Factory.createNewPagamentoDTOWithInvalidData();
+        String jsonRequestBody = objectMapper.writeValueAsString(pagamentoDTO);
+        mockMvc.perform(post("/pagamentos")
+                        .content(jsonRequestBody)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isUnprocessableEntity());
+
+    }
+
+    @Test
+    public void updateShouldUpdateAndReturnPagamentoDTOWhenIdExists() throws Exception{
+
+        String jsonBody= objectMapper.writeValueAsString(pagamentoDTO);
+        mockMvc.perform(put("/pagamentos/{id}", existingId)
+                .content(jsonBody)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$.id").exists())
+                .andExpect(jsonPath("$.valor").exists())
+                .andExpect(jsonPath("$.valor").value(pagamentoDTO.getValor()))
+                .andExpect(jsonPath("$.status").exists())
+                .andExpect(jsonPath("$.status").value("CRIADO"))
+                .andExpect(status().is2xxSuccessful());
+    }
+
+    @Test
+    public void updateShouldReturnNotFoundWhenIdDoesNotExists()throws Exception{
+
+        String jsonRequestBody = objectMapper.writeValueAsString(pagamentoDTO);
+        mockMvc.perform(post("/pagamentos", nonExistingId)
+                        .content(jsonRequestBody)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isNotFound());
+
+    }
+
+    @Test
+    public void updateShouldThrowsExceptionWhenInvalidDate() throws Exception{
+
+        pagamentoDTO= Factory.createNewPagamentoDTOWithInvalidData();
+
+        String jsonRequestBody = objectMapper.writeValueAsString(pagamentoDTO);
+        mockMvc.perform(post("/pagamentos", existingId)
+                        .content(jsonRequestBody)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isUnprocessableEntity());
+
+    }
+    @Test
+    public void  deleteShouldReturnNoContentWhenIdExists() throws Exception{
+        mockMvc.perform(delete("/pagamentos/{id}",existingId)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNoContent())
+                .andDo(print());
+    }
+
+    @Test
+    public void deleteShouldReturnNotFoundWhenIdDoesNotExist() throws Exception{
+        mockMvc.perform(delete("/pagamentos/{id}",nonExistingId)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound())
+                .andDo(print());
 
     }
 
--- src/test/java/com/github/egonbot/ms_pagamento/tests/Factory.java
+++ src/test/java/com/github/egonbot/ms_pagamento/tests/Factory.java
@@ -13,17 +13,41 @@ public class Factory {
         return pagamento;
     }
 
-    public static PagamentoDTO createPagamentoDTO(){
+    public static PagamentoDTO createPagamentoDTO() {
 
         Pagamento pagamento = createPagamento();
         return new PagamentoDTO(pagamento);
 
     }
 
-    public static PagamentoDTO createNewPagamentoDTO(){
+    public static PagamentoDTO createNewPagamentoDTO() {
 
         Pagamento pagamento = createPagamento();
         pagamento.setId(null);
         return new PagamentoDTO(pagamento);
     }
+
+    public static PagamentoDTO createNewPagamentoDTOWithRequiredFields() {
+        Pagamento pagamento = createPagamento();
+        pagamento.setId(null);
+        pagamento.setNome(null);
+        pagamento.setNumeroDoCartao(null);
+        pagamento.setValidade(null);
+        pagamento.setCodigoDeSeguranca(null);
+        return new PagamentoDTO(pagamento);
+
+
+    }
+
+    public  static PagamentoDTO createNewPagamentoDTOWithInvalidData(){
+
+        Pagamento pagamento  = createPagamento();
+        pagamento.setId(null);
+        pagamento.setValor(BigDecimal.valueOf(-35.2));
+        pagamento.setPedidoId(null);
+        pagamento.setFormaDePagamentoId(null);
+
+        return new PagamentoDTO(pagamento);
+    }
+
 }
